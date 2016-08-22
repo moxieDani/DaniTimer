c18a32dd4140294993ab3feef4cf7782b6245a37
daniel-kisoon-kwon <moxie2ks@gmail.com>
2016-08-23 00:32:19 +0900

    Separate code for using win32 platform only.

--- inc/DaniTimer.h
+++ inc/DaniTimer.h
@@ -1,7 +1,10 @@
 #ifndef DANI_TIMER_H_DEF
 #define DANI_TIMER_H_DEF
 
+#ifdef WIN32
 #include<Windows.h>
+#else
+#endif
 
 class DaniTimer
 {
@@ -14,9 +17,12 @@ public:
 	double getElapsedTimeMicroSec();
 
 private:
+#ifdef WIN32
 	LARGE_INTEGER frequency;
 	LARGE_INTEGER startCount;
 	LARGE_INTEGER stopCount;
+#else
+#endif
 	double startTimeMicroSec;
 	double stopTimeMicroSec;
 	double currentTimeMicroSec;
--- src/DaniTimer.cpp
+++ src/DaniTimer.cpp
@@ -2,9 +2,12 @@
 
 DaniTimer::DaniTimer()
 {
+#ifdef WIN32
 	QueryPerformanceFrequency(&frequency);
 	startCount.QuadPart = 0;
 	stopCount.QuadPart = 0;
+#else
+#endif
 	startTimeMicroSec = 0;
 	stopTimeMicroSec = 0;
 	currentTimeMicroSec = 0;
@@ -17,25 +20,34 @@ DaniTimer::~DaniTimer()
 
 int DaniTimer::start()
 {
+#ifdef WIN32
 	if (QueryPerformanceCounter(&startCount))
 		startTimeMicroSec = double(startCount.QuadPart) / (double(frequency.QuadPart) / 1000000.0);
+#else
+#endif
 	return startTimeMicroSec == 0 ? 1 : 0;
 }
 
 int DaniTimer::stop()
 {
+#ifdef WIN32
 	if (QueryPerformanceCounter(&stopCount))
 		stopTimeMicroSec = double(stopCount.QuadPart) / (double(frequency.QuadPart) / 1000000.0);
+#else
+#endif
 	return stopTimeMicroSec == 0 ? 1 : 0;
 }
 
 double DaniTimer::getCurrentTimeMicroSec()
 {
+#ifdef WIN32
 	LARGE_INTEGER currentCount;
 	currentCount.QuadPart = 0;
 	QueryPerformanceCounter(&currentCount);
 	if (startTimeMicroSec > 0.0)
 		currentTimeMicroSec = double(currentCount.QuadPart) / (double(frequency.QuadPart) / 1000000.0) - startTimeMicroSec;
+#else
+#endif
 	return currentTimeMicroSec;
 }
 
